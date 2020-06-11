8d977be53e28dddf07b731dc1a80b0d9d2d26dec
saeedhassansolangi <saeedhassansolangi@gmail.com>
2020-06-11 09:33:06 +0500

    its working here

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/file", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use("/uploads" + express.static("uploads"));
+//Step 3:
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // cb("firstArgsIsError","destinationfolder")
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + file.originalname); // creating file names
+  },
+});
+
+// step 5: accepting file types
+
+const fileFilter = (req, file, cb) => {
+  // cb(null,false) // rejecting file
+  // cb(null,true) // accetping file
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/jpg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// fieldname: 'file',
+// originalname: 'images.jpeg',
+// encoding: '7bit',
+// mimetype: 'image/jpeg',
+// destination: 'uploads',
+// filename: '90c01a5a9c4ccd91c3859504f518bdb9',
+// path: 'uploads/90c01a5a9c4ccd91c3859504f518bdb9',
+// size: 13381
+
+// step 4 : setting file size limits
+const upload = multer({
+  storage: storage,
+  limits: {
+    fieldSize: 1024 * 1024 * 5, // file size is 5 MB , 1024 is bites
+  },
+  fileFilter: fileFilter,
+});
+// Step :1 first create a folder with it
+// const upload = multer({ dest: "uploads" });
+
+app.get("/", (req, res) => {
+  res.send("HI");
+});
+
+app.get("/file", (req, res) => {
+  res.render("file");
+});
+
+//Step 2:
+// upload.single ; means we are uploading only single file , profileImage is the name used internally
+// it just a name , we dont need to worry about it
+app.post("/file", upload.single("file"), (req, res) => {
+  const name = req.body.name;
+  console.log(req.file);
+  res.send("hello World");
+});
+
+app.listen(5000);
